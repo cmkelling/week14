85eb9205141bf74782f494a28ff3148f5103adef
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-08-29 15:50:52 -0500

    commit

--- movieapp/src/movieApp.js
+++ movieapp/src/movieApp.js
@@ -2,5 +2,11 @@ import React, { Component } from 'react'
 import './App.css';
 
 export default class movieApp extends Component {
-
+    render () {
+        return (
+            <div className='container'>
+                
+            </div>
+        )
+    }
 }
