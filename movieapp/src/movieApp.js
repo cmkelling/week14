da819e0ae51626b6cfca2372dd6aeeabf433e2e1
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-08-30 07:21:30 -0500

    commit

--- movieapp/src/movieApp.js
+++ movieapp/src/movieApp.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import './App.css';
 import './MovieList'
+import MovieList from './MovieList';
+import './reviewForm.js'
 
 export default class movieApp extends Component {
     render () {
         return (
             <div className='container'>
-
+                <MovieList/>
+                <Form/>
             </div>
         )
     }
